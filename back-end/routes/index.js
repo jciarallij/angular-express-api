f6ee83bab586a74a2c1bce05d9f7f5eb9cc785cd
jciarallij <jciarallij@gmail.com>
2016-04-27 15:08:24 -0400

    update on project

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,34 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/btb';
+
+var db;
+
+MongoClient.connect(mongoUrl, function(error, database){
+	db = database;
+});
 
 /* post search page. */
 router.post('/search', function(req, res, next) {
-  
-	var name = req.body.name;
+  	// We get name from the post request (in this case, from Angular)
+	var individualStudent = req.body.name;
+	db.collection('students').find({name: individualStudent}).toArray(function(error, studentResult){
+		if(studentResult == 0){
+			db.collection('students').insertOne({
+				name: individualStudent
+			})
+			res.json("Sorry, there were no results. We have added " + individualStudent);
+		} else {
+			res.json("Student: " + studentResult[0].name);
+		}
+		
 
-	var dcClass = [
-		'Tristan',
-		'Josh',
-		'Bogdan',
-		'Keith',
-		'Will',
-		'Curtis',
-		'Joe',
-		'Kochan',
-		'Patrick',
-		'Jonathan',
-		'Jeremy'
-	];
-
-	if(dcClass.indexOf(name) > -1){
-		res.json({message: "Hello, " + name + ", you are a student in this class."})
-	} else {
-		res.json({message: "You are not in this class."});
-	}
-
-	// res.json({message: "Success"});
-
+	});
 
 });
+
+router.get('/search', function(req, res, next) {
+	db.collection('students').find().toArray(function(error, studentResult){
+		res.json(studentResult);
+	});
+		
+});
+
 
 module.exports = router;
--- front-end/controller.js
+++ front-end/controller.js
@@ -1,16 +1,11 @@
 var searchApp = angular.module('searchApp', []);
 searchApp.controller('searchController', function($scope, $http){
 
-	
-
-	$scope.search = function(){
-		// $scope.message = "Hello, Josh";
-
-		var apiUrl ="http://localhost:3000/search";
-
-		$http.post(apiUrl, {name : $scope.name})
+	var apiUrl ="http://localhost:3000/search";
+		$scope.message = "Hello, ";
+		$http.get(apiUrl)
 			.then(function successCallback(response){
-				$scope.result = response.data.message;
+				$scope.studentList = response.data;
 				$scope.status = response.status;
 		
 			}, function errorCallback(response){
@@ -18,24 +13,16 @@ searchApp.controller('searchController', function($scope, $http){
 
 			});
 
+		$scope.search = function(){
+			$http.post(apiUrl, {name: $scope.who})
+			.then(function successCallback(response){
+				$scope.result = response.data;
+				$scope.status = response.status;
 		
-				
+			}, function errorCallback(response){
+				$scope.result = "ERROR!!!";
 
-
-
-
-
-			// .success(function(result, status){
-				// if(result.message == "Success"){
-				// 	$scope.result = "Congratulations! You connected to the webserver!"
-				// 	$scope.status = status;
-				// } else {
-				// 	$scope.status = status;
-				// }
-				// // $scope.status = status;
-				// // $scope.result = result;
-			// });
-
-	}
+			});
+		}
 
 });
