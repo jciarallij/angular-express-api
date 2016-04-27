3c030c5118612918de524bd907cb7ed1d04ebf5c
jciarallij <jciarallij@gmail.com>
2016-04-27 13:35:43 -0400

    Update on the mongodb , express and node app.

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* post search page. */
+router.post('/search', function(req, res, next) {
+  
+	var name = req.body.name;
+
+	var dcClass = [
+		'Tristan',
+		'Josh',
+		'Bogdan',
+		'Keith',
+		'Will',
+		'Curtis',
+		'Joe',
+		'Kochan',
+		'Patrick',
+		'Jonathan',
+		'Jeremy'
+	];
+
+	if(dcClass.indexOf(name) > -1){
+		res.json({message: "Hello, " + name + ", you are a student in this class."})
+	} else {
+		res.json({message: "You are not in this class."});
+	}
+
+	// res.json({message: "Success"});
+
+
 });
 
 module.exports = router;
--- front-end/controller.js
+++ front-end/controller.js
@@ -4,7 +4,38 @@ searchApp.controller('searchController', function($scope, $http){
 	
 
 	$scope.search = function(){
-		$scope.message = "Hello, Josh";
+		// $scope.message = "Hello, Josh";
+
+		var apiUrl ="http://localhost:3000/search";
+
+		$http.post(apiUrl, {name : $scope.name})
+			.then(function successCallback(response){
+				$scope.result = response.data.message;
+				$scope.status = response.status;
+		
+			}, function errorCallback(response){
+				$scope.result = "ERROR!!!";
+
+			});
+
+		
+				
+
+
+
+
+
+			// .success(function(result, status){
+				// if(result.message == "Success"){
+				// 	$scope.result = "Congratulations! You connected to the webserver!"
+				// 	$scope.status = status;
+				// } else {
+				// 	$scope.status = status;
+				// }
+				// // $scope.status = status;
+				// // $scope.result = result;
+			// });
+
 	}
 
 });
